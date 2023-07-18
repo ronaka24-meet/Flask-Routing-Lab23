68021c76baafc5d3c8c124496fa76abf3734550e
ronaka24-meet <139688437+ronaka24-meet@users.noreply.github.com>
2023-07-18 16:25:58 +0300

    MEOW

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -9,8 +9,13 @@ app = Flask(  # Create a flask app
 
 # Your code should be below
 
+@app.route('/')
+def home_page():
+	return render_template('home.html')
 
-
+@app.route('/product')
+def product_page():
+	return render_template('product.html')
 
 # Your code should be above
 
